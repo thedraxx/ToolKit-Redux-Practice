a3ce3916e43c5e7047757ae4ff69a37c96389983
Frank <55867157+thedraxx@users.noreply.github.com>
2022-06-18 20:41:05 -0300

    added useSelector and added more functions in PokemonApp

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getPokemons } from "./store/slices/pokemon";
 
 export const PokemonApp = () => {
   // useDispatch() se utiliza para ejecutar acciones en el store
   const dispatch = useDispatch();
 
+  // useSelector() se utiliza para obtener el estado del store
+  const { page, pokemons, isloading } = useSelector((state) => state.pokemons);
+
   useEffect(() => {
     //disparamos la accion asyncrona de cargar los pokemones en el thunk
-    // al usar una api no podemos usar el dispatch directamente, por eso usamos useDispatch()
+    // al usar una api no podemos usar el dispatch directamente al Slice, por eso hacemos dispatch al thunk, para eso usamos useDispatch()
     // y al ser una peticion asyncrona usamos useEffect() para que se ejecute cuando se termine la peticion
     dispatch(getPokemons());
   }, []);
@@ -18,11 +21,19 @@ export const PokemonApp = () => {
       <hr />
       <h1>Pokemon App</h1>
       <hr />
-      <ul>
-        <li>dasdas</li>
-        <li>dasdas</li>
-        <li>dasdas</li>
-      </ul>
+      <span>Loading: {!isloading ? "True" : "false"}</span>
+
+      {pokemons.map((pokemon) => {
+        return (
+          <ul>
+            <li key={pokemon.name}>{pokemon.name}</li>
+          </ul>
+        );
+      })}
+
+      <button disabled={isloading} onClick={() => dispatch(getPokemons(page))}>
+        Next
+      </button>
     </>
   );
 };
--- src/api/pokemonApi.js
+++ src/api/pokemonApi.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const pokemonApi = axios.create({
+    // usamos el endpoint de la api de pokemon, pero solo la parte que sabemos que no va a cambiar nunca
     baseURL: 'https://pokeapi.co/api/v2',
 });
