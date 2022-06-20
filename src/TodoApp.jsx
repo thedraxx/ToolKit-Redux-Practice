e75486e5fd6eeec0ae5d304743fd01c082e3df50
Frank <55867157+thedraxx@users.noreply.github.com>
2022-06-19 21:04:17 -0300

    consume api in RKTQuery

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { useGetTodosQuery } from "./store/apis";
 
 export const TodoApp = () => {
+  const { data: todos = [], isLoading, isFetching } = useGetTodosQuery();
+  console.log();
   return (
     <>
       <h1>Todo - RTK Query</h1>
       <hr />
-      <h4>IsLoading...</h4>
+      <h4>Loading: {isLoading ? "true" : "false"}</h4>
       <pre>...</pre>
+      <ul>
+        {todos.map((todo) => (
+          <li>
+            {todo.title}
+            <strong>{todo.completed ? " (completed)" : "(incomplete)"} </strong>
+          </li>
+        ))}
+      </ul>
       <button>Next Todo</button>
     </>
   );
--- src/store/apis/todosApi.js
+++ src/store/apis/todosApi.js
@@ -1,5 +1,5 @@
 
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const todosApi = createApi({
 
