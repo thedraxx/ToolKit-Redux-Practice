d42d22ccfdfb5d300181bef3312a420bb070a397
Frank <55867157+thedraxx@users.noreply.github.com>
2022-06-19 21:17:49 -0300

    obtain TODO for ID RTK Query

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,24 +1,38 @@
-import React from "react";
-import { useGetTodosQuery } from "./store/apis";
+import React, { useState } from "react";
+import { useGetTodoQuery, useGetTodosQuery } from "./store/apis";
 
 export const TodoApp = () => {
-  const { data: todos = [], isLoading, isFetching } = useGetTodosQuery();
-  console.log();
+  const [todoId, setTodoId] = useState(1);
+
+  //Esto viene de todosApi.js
+  //   const { data: todos = [], isLoading } = useGetTodosQuery();
+  const { data: todo, isLoading } = useGetTodoQuery(todoId);
+
+  const nextTodo = () => {
+    setTodoId(todoId + 1);
+  };
+
+  const prevTodo = () => {
+    if (todoId === 1) return;
+    setTodoId(todoId - 1);
+  };
+
   return (
     <>
       <h1>Todo - RTK Query</h1>
       <hr />
       <h4>Loading: {isLoading ? "true" : "false"}</h4>
-      <pre>...</pre>
-      <ul>
+      <pre>{JSON.stringify(todo)}</pre>
+      <button onClick={nextTodo}>Next Todo</button>
+      <button onClick={prevTodo}>Previus Todo</button>
+      {/* <ul>
         {todos.map((todo) => (
           <li>
             {todo.title}
             <strong>{todo.completed ? " (completed)" : "(incomplete)"} </strong>
           </li>
         ))}
-      </ul>
-      <button>Next Todo</button>
+      </ul> */}
     </>
   );
 };
--- src/store/apis/todosApi.js
+++ src/store/apis/todosApi.js
@@ -2,18 +2,23 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const todosApi = createApi({
-
+    // RTK Query sirve para mantener cosas en cache y no andar cargando cosas a cada rato que ya hizo
     reducerPath: 'todos',
 
     baseQuery: fetchBaseQuery({
+        //Endpoint que no se modifica cuando hacemos peticiones a la API, osea la parte fija
         baseUrl: 'https://jsonplaceholder.typicode.com',
     }),
 
     endpoints: (builder) => ({
         getTodos: builder.query({
+            // endpoint que se modifica cuando hacemos peticiones a la API, osea la parte variable
             query: () => '/todos'
+        }),
+        getTodo: builder.query({
+            query: (todoId) => `/todos/${todoId}`
         }),
     })
 })
 
-export const { useGetTodosQuery } = todosApi;
+export const { useGetTodosQuery, useGetTodoQuery } = todosApi;
